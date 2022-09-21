086c8796c91f9a67bd3f66494e14b3a9c0420988
Toky Chanthavong <tchantha01@gmail.com>
2022-09-21 16:04:10 -0600

    port change

--- .vscode/client/main.js
+++ .vscode/client/main.js
@@ -1,7 +1,7 @@
 const housesContainer = document.querySelector('#houses-container')
 const form = document.querySelector('form')
 
-const baseURL = `http://localhost:4060/api/houses`
+const baseURL = `http://localhost:4040/api/houses`
 
 const housesCallback = ({ data: houses }) => displayHouses(houses)
 const errCallback = err => console.log(err)
