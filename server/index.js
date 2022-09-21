cd5556731b204379088ddb4dd6954b0d99ab0800
Toky Chanthavong <tchantha01@gmail.com>
2022-09-21 15:26:11 -0600

    updated real estate lab

--- server/index.js
+++ server/index.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const cors = require('cors');
+const path = require('path')
 
+require('dotenv').config()
+
+const port = process.env.PORT || 5050
 const app = express();
 
 app.use(express.json())
@@ -16,8 +20,11 @@ app.put('/api/houses/:id', updateHouse)
 
 
 
+app.use('/', express.static(path.join(__dirname, '../client/index.html')))
+app.use(express.static(path.join(__dirname, '../client')))
 
 
 
 
-app.listen(4004, () => {console.log('Listening on port 4004')})
+app.listen(port, () => {
+    console.log('Docked at port ' + port)})
